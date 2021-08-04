392f8e99b4fae4e58307a9dcd014b3431db33c5a
JoaoRafa19 <joaopedrorafael19@gmail.com>
2021-08-04 20:27:31 -0300

    l10n

--- lib/apps/android.app.dart
+++ lib/apps/android.app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:imc_bloc/generated/l10n.dart';
 import 'package:imc_bloc/view/android/home.page.dart';
 
 class AndroidMyApp extends StatelessWidget {
@@ -7,6 +9,12 @@ class AndroidMyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      supportedLocales: [],
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/apps/ios.app.dart
+++ lib/apps/ios.app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:imc_bloc/generated/l10n.dart';
 
 class IOSMyApp extends StatelessWidget {
   @override
@@ -9,6 +11,11 @@ class IOSMyApp extends StatelessWidget {
       theme: CupertinoThemeData(
         primaryColor: CupertinoColors.activeBlue
       ),
+      localizationsDelegates: [
+        S.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
       home: Container(),
     );
   }
